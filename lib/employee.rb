d69da4f9e32425b29f3911e6b2b88162c9e62c26
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-02-14 22:34:35 +0000

    Completed exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :store_id, presence: true
+  validates :hourly_rate, :inclusion => { :in => [40,200] }
   belongs_to :store
 end
